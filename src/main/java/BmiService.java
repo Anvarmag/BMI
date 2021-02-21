39c063413204cecee162bfa2010ef09aa441b353
Anvarmag <Anvarfon@gmail.com>
2021-02-22 00:01:16 +0300

    pom.xml add

--- src/main/java/BmiService.java
+++ src/main/java/BmiService.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class BmiService {
-}
+public class BmiService {
+        int calculate(double masskg, double growthm) {
+        int bmi = (int) (masskg / (growthm / 100 * growthm / 100 ));
+            int norm = 24;
+            if (bmi == norm) {
+                System.out.println("norm");
+            } else {
+                System.out.println("bad");
+            }
+        return bmi;
+        }
+    }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int bmi = service.calculate(82,182);
+        System.out.println(bmi);
+    }
 }
