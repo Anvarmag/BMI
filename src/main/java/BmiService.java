688e79787ad69f72cfcf29f7c9233013da064b54
Anvarmag <Anvarfon@gmail.com>
2021-03-05 19:55:50 +0300

    fixed #3

--- src/main/java/BmiService.java
+++ src/main/java/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     double calculate(double masskg, double growthm) {
-        double bmi = (double) (masskg / (growthm / 100 * growthm / 100));
+        int bmi = (int) (masskg / (growthm / 100 * growthm / 100));
         return bmi;
     }
 }
